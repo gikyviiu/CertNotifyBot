ec18e43f02892edc4d81d4d7ad62e7abc981af0b
Diana Kozyreva <162029750+gikyviiu@users.noreply.github.com>
2026-09-19 00:07:09 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Telegram
+BOT_TOKEN = ""  
+CHANNEL_ID = ""  
+
+# Database
+DB_HOST = ""  
+DB_USER = ""  
+DB_PASSWORD = ""
+DB_NAME = "" 
+
+# WepApp
+# Логин и пароль для доступа к веб-панели
+PANEL_LOGIN = ""
+PANEL_PASSWORD = ""
